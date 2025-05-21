9985b901e6ff59d9c2a9cf3e29f3fdadcdb275a5
akmal-fahreza <rezaamay17@gmail.com>
2025-05-21 14:21:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Selamat Datang Di Web Informatika Akmal")
-st.write("Belajar Informatika Asik Dan Seru!")
+st.write("Belajar Informatika Seru!")
 st.image("IMG-20250515-WA0014.jpg")
